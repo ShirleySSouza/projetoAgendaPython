3f0790bc3fe49f8fa0636f879aa5e18c0b366634
Shirley Souza <ssouza.shirley@hotmail.com>
2023-12-27 12:22:06 -0300

    Estética, espaço entre blocos

--- manipulaAluno.py
+++ manipulaAluno.py
@@ -1,27 +1,25 @@
 def cadastrar(escola): 
     # O nome dentro da função pode ser qualquer um, inclusive o memso nome da variavel que nomeia o deicionário.
     lista = {}
-    lista['nome'] = input('Digite o nome do aluno: ')
+    lista['nome'] = input('\nDigite o nome do aluno: ')
     lista['disciplina'] = input('Digite a disciplina: ')
     lista['nota1'] = float(input('Digite a nota da primeira prova: '))
     lista['nota2'] = float(input('Digite a nota da segunda prova: '))
     lista['nota3'] = float(input('Digite a nota da terceira prova: '))
-    lista['nota4'] = float(input('Digite a nota da última prova: '))
+    lista['nota4'] = float(input('Digite a nota da última prova: ' ))
     escola.append(lista)
     
 def listar(escola):
-        
-    for aluno in escola:
-        print(f"As notas do aluno {aluno['nome']} na disciplina {aluno['disciplina']} são {aluno['nota1']}, {aluno['nota2']}, {aluno['nota3']} e {aluno['nota4']}.")
-        media =  ((aluno['nota1'] + aluno['nota2'] + aluno['nota3'] + aluno['nota4']) /4)  
+    for posicao in escola:
+        print(f"\nAs notas do aluno {posicao['nome']} na disciplina {posicao['disciplina']} são {posicao['nota1']}, {posicao['nota2']}, {posicao['nota3']} e {posicao['nota4']}.")
+        media = ((posicao['nota1'] + posicao['nota2'] + posicao['nota3'] + posicao['nota4']) /4)  
         if media >= 7:
-            print(f"A media do aluno {aluno['nome']} é {media} e o aluno está aprovado.")
+            print(f"A media do aluno {posicao['nome']} é {media} e o aluno está aprovado.")
         else:
-            print(f"A media do aluno {aluno['nome']} é {media} e o aluno está reprovado.")
-            
+            print(f"A media do aluno {posicao['nome']} é {media} e o aluno está reprovado.")
+        
 def buscar(escola):
-    
-    pesquisa = input('Digite o nome do aluno: ')
+    pesquisa = input('\nDigite o nome do aluno: ')
     posicao = -1
     i = 0
     for aluno in escola:
@@ -33,26 +31,26 @@ def buscar(escola):
 
 def exibir(escola, posicao):
     if posicao > -1:    
-        print(f"As notas do aluno {escola[posicao]['nome']} na disciplina {escola[posicao]['disciplina']} são {escola[posicao]['nota1']}, {escola[posicao]['nota2']}, {escola[posicao]['nota3']} e {escola[posicao]['nota4']}.")
+        print(f"\nAs notas do aluno {escola[posicao]['nome']} na disciplina {escola[posicao]['disciplina']} são {escola[posicao]['nota1']}, {escola[posicao]['nota2']}, {escola[posicao]['nota3']} e {escola[posicao]['nota4']}.")
     else:
         print('Nome não encontrado.')
         return posicao
     
 def alterar(escola, posicao):
     if posicao > -1:
-        escola[posicao]['nome'] = input('Digite o nome do aluno: ')
+        escola[posicao]['nome'] = input('\nDigite o nome do aluno: ')
         escola[posicao]['disciplina'] = input('Digite a disciplina: ')
         escola[posicao]['nota1'] = float(input('Digite a nota da primeira prova: '))
         escola[posicao]['nota2'] = float(input('Digite a nota da segunda prova: '))
         escola[posicao]['nota3'] = float(input('Digite a nota da terceira prova: '))
         escola[posicao]['nota4'] = float(input('Digite a nota da última prova: '))
     else:
-        print('Não encontrado.')
+        print('\nNão encontrado.')
         return posicao
     
 def excluir(escola, posicao):
     if posicao > -1:
-        print(f"O aluno {escola[posicao]['nome']} foi removido da lista.")
+        print(f"\nO aluno {escola[posicao]['nome']} foi removido da lista.")
         escola.pop(posicao)
     else:
         print('Nome não encontrado.')
--- uteis.py
+++ uteis.py
@@ -1,3 +1,3 @@
 def opcoes():
     
-    return int(input('Menu de opções:\n1 - Cadastrar aluno.\n2 - Listar informações cadastradas\n3 - Pesquisar pelo nome.\n4 - Alterar cadastro.\n5 - Excluir cadastro.\n9 - Sair.\nDigite a opção desejada: '))
+    return int(input('\nMenu de opções:\n1 - Cadastrar aluno.\n2 - Listar informações cadastradas\n3 - Pesquisar pelo nome.\n4 - Alterar cadastro.\n5 - Excluir cadastro.\n9 - Sair.\nDigite a opção desejada: '))
